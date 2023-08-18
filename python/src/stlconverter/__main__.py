89c5311fe0a26f1b45d1d8ae33a7ecdc2e29fb41
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2023-08-19 01:30:09 +0200

    Add docstrings and comments

--- python/src/stlconverter/__main__.py
+++ python/src/stlconverter/__main__.py
@@ -1,12 +1,28 @@
+"""Main execution module.
+
+This module is the entry point for the STLConverter application through the
+command line interface.
+
+Author:
+    Paulo Sanchez (@erlete)
+"""
+
+
 import sys
 from time import perf_counter as pc
 
 from colorama import Fore, Style, init
-from modules.conversion import STL
-from modules.input_handling import InputHandler
+from .modules.conversion import STL
+from .modules.input_handling import InputHandler
 
 
-def same_format_warn(input_handler, stl):
+def same_format_warn(input_handler: InputHandler, stl: STL) -> None:
+    """Print a warning if input and output formats are the same.
+
+    Args:
+        input_handler (InputHandler): Input handler instance.
+        stl (STL): STL instance.
+    """
     if input_handler.is_binary and stl._is_input_binary:
         print(
             Style.BRIGHT + Fore.YELLOW
@@ -23,7 +39,7 @@ def same_format_warn(input_handler, stl):
         )
 
 
-init()
+init()  # Initialize colorama.
 handler = InputHandler(sys.argv[1:])
 out_name = (
     f"{handler.input_path[:handler.input_path.rfind('.')]}-converted"
@@ -38,12 +54,12 @@ try:
     else:
         same_format_warn(handler, stl)
         stl.save_stla(out_name)
-except Exception:
+except Exception:  # Uncaught exceptions.
     print(
         Style.BRIGHT + Fore.RED
         + f"Unknown error: {Style.RESET_ALL}{str(sys.exc_info()[1])}"
     )
-else:
+else:  # Successful conversion.
     mode = "binary" if handler.is_binary else "ASCII"
     print(
         Style.BRIGHT + Fore.GREEN

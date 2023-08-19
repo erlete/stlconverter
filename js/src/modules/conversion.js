b6fa40db4e458328ea4811e4bae9ce6473f7d837
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2023-08-19 17:41:41 +0200

    Convert classes to plain functions and rename them

--- js/src/modules/conversion.js
+++ js/src/modules/conversion.js
@@ -10,135 +10,129 @@ function arrayBufferToString(buffer) {
     return (new TextDecoder("utf-8")).decode(buffer);
 }
 
-class STLTriangleReader {
-    read_stlb(data) {
-        this.normal = parseBytes(new Float32Array(data.slice(0, 12).buffer));
+function readSTLbTriangle(data) {
+    normal = parseBytes(new Float32Array(data.slice(0, 12).buffer));
 
-        this.vertices = [
-            parseBytes(new Float32Array(data.slice(12, 24).buffer)),
-            parseBytes(new Float32Array(data.slice(24, 36).buffer)),
-            parseBytes(new Float32Array(data.slice(36, 48).buffer))
-        ];
+    vertices = [
+        parseBytes(new Float32Array(data.slice(12, 24).buffer)),
+        parseBytes(new Float32Array(data.slice(24, 36).buffer)),
+        parseBytes(new Float32Array(data.slice(36, 48).buffer))
+    ];
 
-        this.attribute = new Uint16Array(data.slice(48, 50).buffer)[0];
+    attribute = new Uint16Array(data.slice(48, 50).buffer)[0];
 
-        return {
-            normal: this.normal,
-            vertices: this.vertices,
-            attribute: this.attribute
-        }
-    }
-
-    read_stla(data) {
-        this.normal = data[0].replace('facet normal ', '').split(' ').map(parseFloat);
-
-        this.vertices = [
-            data[2].replace('vertex ', '').split(' ').map(parseFloat),
-            data[3].replace('vertex ', '').split(' ').map(parseFloat),
-            data[4].replace('vertex ', '').split(' ').map(parseFloat)
-        ];
-
-        this.attribute = 0;
-
-        return {
-            normal: this.normal,
-            vertices: this.vertices,
-            attribute: this.attribute
-        }
+    return {
+        normal: normal,
+        vertices: vertices,
+        attribute: attribute
     }
 }
 
-class STLFileReader {
-    read_stlb(data) {
-        this.header = data.slice(0, 80);
-        this.header = arrayBufferToString(this.header.slice(0, this.header.indexOf(0)));
+function readSTLaTriangle(data) {
+    normal = data[0].replace('facet normal ', '').split(' ').map(parseFloat);
 
-        this.n_triangles = new Uint32Array(data.slice(80, 84).buffer)[0];
+    vertices = [
+        data[2].replace('vertex ', '').split(' ').map(parseFloat),
+        data[3].replace('vertex ', '').split(' ').map(parseFloat),
+        data[4].replace('vertex ', '').split(' ').map(parseFloat)
+    ];
 
-        this.triangles = [];
-        const triangle_reader = new STLTriangleReader();
-        for (let i = 0; i < this.n_triangles; i++) {
-            let triangle = data.slice(84 + i * 50, 84 + (i + 1) * 50);
-            this.triangles.push(triangle_reader.read_stlb(triangle));
-        }
+    attribute = 0;
 
-        return {
-            header: this.header,
-            n_triangles: this.n_triangles,
-            triangles: this.triangles
-        }
-    }
-
-    read_stla(data) {
-        data = arrayBufferToString(data);
-        const lines = data.split('\n').map(line => line.trim());
-
-        this.header = lines[0].replace('solid ', '');
-
-        this.n_triangles = lines.filter(line => line.startsWith('facet')).length;
-
-        this.triangles = [];
-        const triangle_reader = new STLTriangleReader();
-        for (let i = 0; i < this.n_triangles; i++) {
-            let triangle = lines.slice(1 + i * 7, 1 + (i + 1) * 7);
-            this.triangles.push(triangle_reader.read_stla(triangle));
-        }
-
-        return {
-            header: this.header,
-            n_triangles: this.n_triangles,
-            triangles: this.triangles
-        }
+    return {
+        normal: normal,
+        vertices: vertices,
+        attribute: attribute
     }
 }
 
-class STL {
-    constructor(file) {
-        const reader = new FileReader();
+function readSTLbFile(data) {
+    header = data.slice(0, 80);
+    header = arrayBufferToString(header.slice(0, header.indexOf(0)));
 
-        reader.onload = function (e) {
-            const text = e.target.result;
-            const bytes = new Uint8Array(text);
+    n_triangles = new Uint32Array(data.slice(80, 84).buffer)[0];
 
-            if (compareArrays(parseBytes(bytes.slice(0, 5)), [115, 111, 108, 105, 100])) {
-                console.log('ascii');
-                this.data = (new STLFileReader).read_stla(text);
-            } else {
-                console.log('binary');
-                this.data = (new STLFileReader).read_stlb(bytes);
-            }
-        };
-
-        reader.readAsArrayBuffer(file);
+    triangles = [];
+    const triangle_reader = new STLTriangleReader();
+    for (let i = 0; i < n_triangles; i++) {
+        let triangle = data.slice(84 + i * 50, 84 + (i + 1) * 50);
+        triangles.push(triangle_reader.readSTLbTriangle(triangle));
     }
 
-    save_stlb() {
-        let data = this.data.header;
-        data += this.data.n_triangles;
-        for (let i = 0; i < this.data.n_triangles; i++) {
-            data += this.data.triangles[i].normal;
-            for (let j = 0; j < 3; j++) {
-                data += this.data.triangles[i].vertices[j];
-            }
-            data += this.data.triangles[i].attribute;
+    return {
+        header: header,
+        n_triangles: n_triangles,
+        triangles: triangles
+    }
+}
+
+function readSTLaFile(data) {
+    data = arrayBufferToString(data);
+    const lines = data.split('\n').map(line => line.trim());
+
+    header = lines[0].replace('solid ', '');
+
+    n_triangles = lines.filter(line => line.startsWith('facet')).length;
+
+    triangles = [];
+    const triangle_reader = new STLTriangleReader();
+    for (let i = 0; i < n_triangles; i++) {
+        let triangle = lines.slice(1 + i * 7, 1 + (i + 1) * 7);
+        triangles.push(triangle_reader.readSTLaTriangle(triangle));
+    }
+
+    return {
+        header: header,
+        n_triangles: n_triangles,
+        triangles: triangles
+    }
+}
+
+function readSTL(file) {
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+        const text = e.target.result;
+        const bytes = new Uint8Array(text);
+
+        if (compareArrays(parseBytes(bytes.slice(0, 5)), [115, 111, 108, 105, 100])) {
+            console.log('ascii');
+            data = readSTLaFile(text);
+        } else {
+            console.log('binary');
+            data = readSTLbFile(bytes);
         }
+    };
 
-        return data;
-    }
+    reader.readAsArrayBuffer(file);
+}
 
-    save_stla() {
-        let data = 'solid ' + this.data.header + '\n';
-        for (let i = 0; i < this.data.n_triangles; i++) {
-            data += '  facet normal ' + this.data.triangles[i].normal + '\n';
-            data += '    outer loop\n';
-            for (let j = 0; j < 3; j++) {
-                data += '      vertex ' + this.data.triangles[i].vertices[j] + '\n';
-            }
-            data += '    endloop\n';
-            data += '  endfacet\n';
+function saveSTLb() {
+    let data = data.header;
+    data += data.n_triangles;
+    for (let i = 0; i < data.n_triangles; i++) {
+        data += data.triangles[i].normal;
+        for (let j = 0; j < 3; j++) {
+            data += data.triangles[i].vertices[j];
         }
-        data += 'endsolid ' + this.data.header + '\n';
-
-        return data;
+        data += data.triangles[i].attribute;
     }
+
+    return data;
+}
+
+function saveSTLa() {
+    let data = 'solid ' + data.header + '\n';
+    for (let i = 0; i < data.n_triangles; i++) {
+        data += '  facet normal ' + data.triangles[i].normal + '\n';
+        data += '    outer loop\n';
+        for (let j = 0; j < 3; j++) {
+            data += '      vertex ' + data.triangles[i].vertices[j] + '\n';
+        }
+        data += '    endloop\n';
+        data += '  endfacet\n';
+    }
+    data += 'endsolid ' + data.header + '\n';
+
+    return data;
 }

a8d526fa8409b3b34308a2edff5a4d7a65e806fd
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2023-08-20 00:13:38 +0200

    Reformat and document index

--- js/src/index.js
+++ js/src/index.js
@@ -1,22 +1,19 @@
 /**
- * Get results from input data.
- * @date 8/12/2023 - 3:47:08 AM
+ * Read an STL file, detect its type and convert it to transition data structure.
+ * @date 8/20/2023 - 12:09:25 AM
  *
- * @param {Event} event - Drop event.
+ * @param {Event} event - File dropping event.
  */
-function getResults(event) {
+function readFile(event) {
     event.preventDefault();
-
     const file = event.dataTransfer.files[0];
     readSTL(file);
 }
 
-function resetView() {}
+/**
+ * Reset dynamic elements' state.
+ * @date 8/20/2023 - 12:09:49 AM
+ */
+function resetView() {
 
-function submit1() {
-    saveSTLb();
-}
-
-function submit2() {
-    saveSTLa();
 }
